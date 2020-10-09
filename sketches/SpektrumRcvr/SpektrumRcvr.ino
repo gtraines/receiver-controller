3e966765399562d0a7c4e01cb9d7b169db307cf3
gtraines <gtraines@hotmail.com>
2020-10-08 22:44:13 -0700

    Trblsht Serial pins

--- sketches/SpektrumRcvr/SpektrumRcvr.ino
+++ sketches/SpektrumRcvr/SpektrumRcvr.ino
@@ -2,14 +2,16 @@
  * Example code for Arduino Micro. For bind feature on other boards 
  * please power your receiver directly from arduino pin or via MOSFET
  */
-
+#include <Arduino.h>
+#include <HardwareSerial.h>
 #include <SpektrumSerialReceiver.h>
 
-SpektrumSerialReceiver RX(0);                         // Creating receiver object with bind pin 0 (same as RX1 on Arduino Micro)
+SpektrumSerialReceiver RX;                         // Creating receiver object with bind pin 0 (same as RX1 on Arduino Micro)
 
 void setup() {
   pinMode(3, INPUT_PULLUP);                           // Button for binding
-  if (!digitalRead(3)) RX.bindRX(DSMX_11ms);          // Binding in selected mode (DSM2_22ms/DSM2_11ms/DSMX_22ms/DSMX_11ms)
+  RX = SpektrumSerialReceiver();
+  if (!digitalRead(3)) RX.bindRX(DSMX_22ms);          // Binding in selected mode (DSM2_22ms/DSM2_11ms/DSMX_22ms/DSMX_11ms)
   Serial.begin(115200);                               // Serial port for monitoring data on PC
   RX.begin(Serial1);                                  // Starting getting data from receiver on Serial1
 
@@ -18,7 +20,7 @@ void setup() {
 void loop() {
   RX.update();                                        // Updating channel values
   
-  Serial.print(RX.getChannel(0));                     // Getting channel 0 (Throttle) data
+  Serial.print(RX.getChannel(1));                     // Getting channel 0 (Throttle) data
   Serial.print('\t');
   Serial.print(RX.getRXLosses());                     // Getting signal losses
   Serial.print('\t');
@@ -33,7 +35,7 @@ void loop() {
       Serial.println("DSMX 22ms");
       break;
     case DSMX_11ms:
-      Serial.println("DSMX 22ms");
+      Serial.println("DSMX 11ms");
       break;
     default:
       Serial.println("No connection");
